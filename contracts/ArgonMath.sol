9f33a5168842b671ba0963ae5a636215db2ae741
infinitywarg <ganesh.k.anantwar@gmail.com>
2022-06-22 18:56:47 +0530

    add percent function

--- contracts/ArgonMath.sol
+++ contracts/ArgonMath.sol
@@ -4,7 +4,7 @@ pragma solidity =0.8.8;
 /// @title Simple Solidity library for 18-decimal fixed point math
 /// @author @infinitywarg for @infinitywarp Labs
 /// @notice Fixed point math operations with 18-decimals for DeFi applications
-/// @dev By restricting the operands to +/- 10^18 with 18-decimals i.e. 10^36, overflow/underflow never occcurs for 256-bit signed integers
+/// @dev By restricting the inputs to +/- 10^18 with 18-decimals i.e. 10^36, overflow/underflow never occcurs for 256-bit signed integers
 /// @dev 10^36 * 10^36 = 10*72 always less than 2^255-1
 /// @dev For most applications, 10^18 i.e. 1 million trillion whole units is a large enough amount
 
@@ -12,6 +12,7 @@ contract ArgonMath {
     int256 private constant MAX = 1e36;
     int256 private constant MIN = -1e36;
     int256 internal constant UNITY = 1e18;
+    int256 internal constant HUNDRED = 1e20;
     int256 internal constant PI = 3_141592_653589_793238;
     int256 internal constant E = 2_718281_828459_045235;
     int256 internal constant LOG2E = 1_442695_040888_963407;
@@ -23,8 +24,9 @@ contract ArgonMath {
     error Argon__NegativeLog(int256 x);
 
     /// @notice Modifier to check for overflow and underflow
-    /// @dev Maximum Number = 999,999,999,999.999999, Minimum Number = -999,999,999,999.999999
-    /// @param x First operand in mathematical operation
+    /// @dev Maximum Decimal Value = 999999999999999999.999999999999999999
+    /// @dev Minimum Decimal Value = - 999999999999999999.999999999999999999
+    /// @param x Input in mathematical operation
 
     modifier check(int256 x) {
         if (x >= MAX) {
@@ -84,6 +86,18 @@ contract ArgonMath {
         }
         unchecked {
             result = (x * UNITY) / y;
+        }
+    }
+
+    function percent(int256 x, int256 y)
+        internal
+        pure
+        check(x)
+        check(y)
+        returns (int256 result)
+    {
+        unchecked {
+            result = (x * y) / HUNDRED;
         }
     }
 
