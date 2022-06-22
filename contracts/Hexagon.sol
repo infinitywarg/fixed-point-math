01bc673bfb92bbe358d08b0d08e10a2d4407d1db
infinitywarg <ganesh.k.anantwar@gmail.com>
2022-06-22 17:28:46 +0530

    add all required functions

--- contracts/Hexagon.sol
+++ contracts/Hexagon.sol
@@ -9,28 +9,114 @@ pragma solidity =0.8.8;
 contract Hexagon {
     int128 private constant MAX_INT = 1e18;
     int128 private constant MIN_INT = -1e18;
-    int128 private constant SCALE = 1e6;
+    int128 private constant ADJ_18 = 1e12;
+    int128 internal constant UNITY = 1e6;
+    int128 internal constant PI = 3_141593;
+    int128 internal constant E = 2_718282;
 
     /// @notice Modifier to check for overflow and underflow
     /// @dev Maximum Number = 999,999,999,999.999999, Minimum Number = -999,999,999,999.999999
     /// @param x First operand in mathematical operation
-    /// @param y Second operand in mathematical operation
 
-    modifier check(int128 x, int128 y) {
-        require(x > MIN_INT && x < MAX_INT && y > MIN_INT && y < MAX_INT);
+    modifier check(int128 x) {
+        require(x > MIN_INT && x < MAX_INT);
         _;
     }
 
-    function mul(int128 x, int128 y) public pure returns (int128 result) {
+    function from18(int256 x) internal pure returns (int128 result) {
         unchecked {
-            result = (x * y) / SCALE;
+            result = int128(x / ADJ_18);
         }
     }
 
-    function div(int128 x, int128 y) public pure returns (int128 result) {
+    function to18(int128 x) internal pure check(x) returns (int256 result) {
+        unchecked {
+            result = x * ADJ_18;
+        }
+    }
+
+    function add(int128 x, int128 y)
+        internal
+        pure
+        check(x)
+        check(y)
+        returns (int128 result)
+    {
+        unchecked {
+            result = x + y;
+        }
+    }
+
+    function sub(int128 x, int128 y)
+        internal
+        pure
+        check(x)
+        check(y)
+        returns (int128 result)
+    {
+        unchecked {
+            result = x - y;
+        }
+    }
+
+    function mul(int128 x, int128 y)
+        internal
+        pure
+        check(x)
+        check(y)
+        returns (int128 result)
+    {
+        unchecked {
+            result = (x * y) / UNITY;
+        }
+    }
+
+    function div(int128 x, int128 y)
+        internal
+        pure
+        check(x)
+        check(y)
+        returns (int128 result)
+    {
         unchecked {
             require(y != 0);
-            result = (x * SCALE) / y;
+            result = (x * UNITY) / y;
         }
+    }
+
+    function sqrt(int128 x) internal pure check(x) returns (int128 result) {
+        unchecked {
+            require(x >= 0);
+        }
+    }
+
+    function ln(int128 x) internal pure check(x) returns (int128 result) {
+        unchecked {
+            require(x >= 0);
+        }
+    }
+
+    function log2(int128 x) internal pure check(x) returns (int128 result) {
+        unchecked {
+            require(x >= 0);
+        }
+    }
+
+    function exp(int128 x) internal pure check(x) returns (int128 result) {
+        unchecked {}
+    }
+
+    function exp2(int128 x) internal pure check(x) returns (int128 result) {
+        unchecked {}
+    }
+
+    function pow(int128 x, int128 y)
+        internal
+        pure
+        check(x)
+        check(y)
+        returns (int128 result)
+    {
+        unchecked {}
     }
 }
