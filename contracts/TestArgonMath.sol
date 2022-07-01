47fcbab91a72f33179af3e8df88966043705dd64
infinitywarg <ganesh.k.anantwar@gmail.com>
2022-07-02 02:53:51 +0530

    test library functions with gas used

--- contracts/TestArgonMath.sol
+++ contracts/TestArgonMath.sol
@@ -2,5 +2,32 @@
 pragma solidity =0.8.8;
 
 import "hardhat/console.sol";
+import "./ArgonMath.sol";
 
-contract TryArgon {}
+contract TestArgonMath {
+    using ArgonMath for uint256;
+
+    function add(uint256 x, uint256 y) public view returns (uint256 n) {
+        uint256 startGas = gasleft();
+        n = x.add(y);
+        console.log(startGas - gasleft());
+    }
+
+    function sub(uint256 x, uint256 y) public view returns (uint256 n) {
+        uint256 startGas = gasleft();
+        n = x.sub(y);
+        console.log(startGas - gasleft());
+    }
+
+    function mul(uint256 x, uint256 y) public view returns (uint256 n) {
+        uint256 startGas = gasleft();
+        n = x.mul(y);
+        console.log(startGas - gasleft());
+    }
+
+    function div(uint256 x, uint256 y) public view returns (uint256 n) {
+        uint256 startGas = gasleft();
+        n = x.div(y);
+        console.log(startGas - gasleft());
+    }
+}
