c7bf82aafac8f15e14b255fc5f0e9ce554ac6f44
infinitywarg <ganesh.k.anantwar@gmail.com>
2022-07-02 02:01:28 +0530

    refactor: max input to 2^128-1

--- contracts/ArgonMath.sol
+++ contracts/ArgonMath.sol
@@ -1,204 +1,240 @@
 //SPDX-License-Identifier: MIT
 pragma solidity =0.8.8;
 
-/// @title Simple Solidity library for 18-decimal fixed point math
-/// @author @infinitywarg for @infinitywarp Labs
-/// @notice Fixed point math operations with 18-decimals for DeFi applications
-/// @dev By restricting the inputs to 10^18 with 18-decimals i.e. 10^36, overflow never occcurs for 256-bit integers
-/// @dev 10^36 * 10^36 = 10*72 always less than 2^255-1
-/// @dev For most applications, 10^18 i.e. 1 million trillion whole units is a large enough amount
-
 contract ArgonMath {
-    uint256 private constant MAX_INPUT = 1e36 - 1;
-    uint256 private constant SCALE = 1e18;
-    uint256 private constant SCALE_SQUARE = 1e36;
-    uint256 private constant SCALE_ROOT = 1e9;
-    uint256 private constant SCALE_HALF = 5 * 1e17;
-    uint256 private constant SCALE_HUNDRED = 1e20;
-    uint256 private constant PI = 3_141592_653589_793238;
-    uint256 private constant E = 2_718281_828459_045235;
-    uint256 private constant LOG2E = 1_442695_040888_963407;
+    uint256 private constant OVERFLOW = 2**128;
+    uint256 private constant SCALING = 1e18;
+    uint256 private constant SCALING_SQUARE = 1e36;
+    uint256 private constant SCALING_ROOT = 1e36;
+    uint256 private constant SCALED_HUNDRED = 1e20;
+    uint256 private constant SCALED_HALF = 5 * 1e17;
+    uint256 private constant SCALED_PI = 3_141592_653589_793238;
+    uint256 private constant SCALED_E = 2_718281_828459_045235;
 
     error Argon__Overflow(uint256 x);
     error Argon__DivideByZero(uint256 x, uint256 y);
-    error Argon__Exp2InputLarge(uint256 x);
-    error Argon__ExpInputLarge(uint256 x);
-    error Argon__LogInputTooSmall(uint256 x);
+    error Argon__LargeExpInput(uint256 x);
+    error Argon__LargeBexpInput(uint256 x);
+    error Argon__LogNegativeResult(uint256 x);
 
-    /// @notice Modifier to check for overflow and underflow
-    /// @dev Maximum Decimal Value = 999999999999999999.999999999999999999
+    /// @notice Modifier to check for overflow
+    /// @dev Maximum Value = (2^128-1)/(10^18) =
+    /// @dev 340,282,366,920,938,463,463.374607431768211455
     /// @param x Input in mathematical operation
-
     modifier check(uint256 x) {
-        if (x > MAX_INPUT) {
+        if (x >= OVERFLOW) {
             revert Argon__Overflow(x);
         }
         _;
     }
 
-    function add(uint256 x, uint256 y) internal pure check(x) check(y) returns (uint256 result) {
+    /// @notice Explain to an end user what this does
+    /// @dev Explain to a developer any extra details
+    /// @param x a parameter just like in doxygen (must be followed by parameter name)
+    /// @param y a parameter just like in doxygen (must be followed by parameter name)
+    /// @return n the return variables of a contract’s function state variable
+    function add(uint256 x, uint256 y) internal pure check(x) check(y) returns (uint256 n) {
         unchecked {
-            result = x + y;
+            n = x + y;
         }
     }
 
-    function sub(uint256 x, uint256 y) internal pure check(x) check(y) returns (uint256 result) {
+    /// @notice Explain to an end user what this does
+    /// @dev Explain to a developer any extra details
+    /// @param x a parameter just like in doxygen (must be followed by parameter name)
+    /// @param y a parameter just like in doxygen (must be followed by parameter name)
+    /// @return n the return variables of a contract’s function state variable
+    function sub(uint256 x, uint256 y) internal pure check(x) check(y) returns (uint256 n) {
         unchecked {
-            result = x - y;
+            n = x - y;
         }
     }
 
-    function mul(uint256 x, uint256 y) internal pure check(x) check(y) returns (uint256 result) {
+    /// @notice Explain to an end user what this does
+    /// @dev Explain to a developer any extra details
+    /// @param x a parameter just like in doxygen (must be followed by parameter name)
+    /// @param y a parameter just like in doxygen (must be followed by parameter name)
+    /// @return n the return variables of a contract’s function state variable
+    function mul(uint256 x, uint256 y) internal pure check(x) check(y) returns (uint256 n) {
         unchecked {
-            result = (x * y) / SCALE;
+            n = (x * y) / SCALING;
         }
     }
 
-    function div(uint256 x, uint256 y) internal pure check(x) check(y) returns (uint256 result) {
+    /// @notice Explain to an end user what this does
+    /// @dev Explain to a developer any extra details
+    /// @param x a parameter just like in doxygen (must be followed by parameter name)
+    /// @param y a parameter just like in doxygen (must be followed by parameter name)
+    /// @return n the return variables of a contract’s function state variable
+    function div(uint256 x, uint256 y) internal pure check(x) check(y) returns (uint256 n) {
         if (y == 0) {
             revert Argon__DivideByZero(x, y);
         }
         unchecked {
-            result = (x * SCALE) / y;
+            n = (x * SCALING) / y;
         }
     }
 
-    function inv(uint256 x) internal pure returns (uint256 result) {
+    /// @notice Explain to an end user what this does
+    /// @dev Explain to a developer any extra details
+    /// @param x a parameter just like in doxygen (must be followed by parameter name)
+    /// @return n the return variables of a contract’s function state variable
+    function inv(uint256 x) internal pure check(x) returns (uint256 n) {
         unchecked {
-            result = SCALE_SQUARE / x;
+            n = SCALING_SQUARE / x;
         }
     }
 
-    function percent(uint256 x, uint256 p) internal pure check(x) check(p) returns (uint256 result) {
+    /// @notice Explain to an end user what this does
+    /// @dev Explain to a developer any extra details
+    /// @param x a parameter just like in doxygen (must be followed by parameter name)
+    /// @param y a parameter just like in doxygen (must be followed by parameter name)
+    /// @return n the return variables of a contract’s function state variable
+    function avg(uint256 x, uint256 y) internal pure check(x) check(y) returns (uint256 n) {
         unchecked {
-            result = (x * p) / SCALE_HUNDRED;
+            n = (x + y) >> 1;
         }
     }
 
+    /// @notice Explain to an end user what this does
+    /// @dev Explain to a developer any extra details
+    /// @param x1 a parameter just like in doxygen (must be followed by parameter name)
+    /// @param y1 a parameter just like in doxygen (must be followed by parameter name)
+    /// @param x2 a parameter just like in doxygen (must be followed by parameter name)
+    /// @param y2 a parameter just like in doxygen (must be followed by parameter name)
+    /// @return n the return variables of a contract’s function state variable
     function wavg(
         uint256 x1,
-        uint256 n1,
+        uint256 y1,
         uint256 x2,
-        uint256 n2
-    ) internal pure returns (uint256 result) {
+        uint256 y2
+    ) internal pure check(x1) check(y1) check(x2) check(y2) returns (uint256 n) {
         unchecked {
-            result = ((x1 * n1) + (x2 * n2)) / (n1 + n2);
+            n = (x1 * y1) / (y1 + y2) + (x2 * y2) / (y1 + y2);
+            n /= SCALING;
         }
     }
 
-    function pi() internal pure returns (uint256 result) {
-        return PI;
-    }
-
-    function e() internal pure returns (uint256 result) {
-        return E;
-    }
-
-    function sqrt(uint256 x) internal pure returns (uint256 result) {
-        if (x == 0) {
-            return 0;
-        }
-        result = sqrtInt(x) * SCALE_ROOT;
-    }
-
-    function log2(uint256 x) internal pure returns (uint256 result) {
-        if (x < SCALE) {
-            revert Argon__LogInputTooSmall(x);
-        }
+    /// @notice Explain to an end user what this does
+    /// @dev Explain to a developer any extra details
+    /// @param x a parameter just like in doxygen (must be followed by parameter name)
+    /// @param y a parameter just like in doxygen (must be followed by parameter name)
+    /// @return n the return variables of a contract’s function state variable
+    function percent(uint256 x, uint256 y) internal pure check(x) check(n) returns (uint256 n) {
         unchecked {
-            uint256 n = _msb(x / SCALE);
-            result = n * SCALE;
-            uint256 y = x >> n;
-            if (y == SCALE) {
-                return result;
-            }
-            for (uint256 delta = SCALE_HALF; delta > 0; delta >>= 1) {
-                y = (y * y) / SCALE;
-                if (y >= 2 * SCALE) {
-                    result += delta;
-                    y >>= 1;
-                }
-            }
+            n = (x * y) / SCALED_HUNDRED;
         }
     }
 
-    function ln(uint256 x) internal pure returns (uint256 result) {
+    /// @notice Explain to an end user what this does
+    /// @dev Explain to a developer any extra details
+    /// @return n the return variables of a contract’s function state variable
+    function pi() internal pure returns (uint256 n) {
+        n = SCALED_PI;
+    }
+
+    /// @notice Explain to an end user what this does
+    /// @dev Explain to a developer any extra details
+    /// @return n the return variables of a contract’s function state variable
+    function e() internal pure returns (uint256 n) {
+        n = SCALED_E;
+    }
+
+    /// @notice Explain to an end user what this does
+    /// @dev Explain to a developer any extra details
+    /// @param x a parameter just like in doxygen (must be followed by parameter name)
+    /// @return n the return variables of a contract’s function state variable
+    function sqrt(uint256 x) public pure check(x) returns (uint256 n) {
         unchecked {
-            result = (log2(x) * SCALE) / LOG2E;
+            n = _sqrtinit(x);
+            n = (n + x / n) >> 1;
+            n = (n + x / n) >> 1;
+            n = (n + x / n) >> 1;
+            n = (n + x / n) >> 1;
+            n = (n + x / n) >> 1;
+            n = (n + x / n) >> 1;
+            n = (n >= (x / n)) ? (x / n) : n;
+            n *= SCALING_ROOT;
         }
     }
 
-    function exp(uint256 x) internal pure check(x) returns (uint256 result) {
-        // e^82.893063347785644620 = 10^36
-        if (x >= 82_893063_347785_644620) {
-            revert Argon__ExpInputLarge(x);
+    /// @notice Explain to an end user what this does
+    /// @dev Explain to a developer any extra details
+    /// @param x a parameter just like in doxygen (must be followed by parameter name)
+    /// @return n the return variables of a contract’s function state variable
+    function lb(uint256 x) internal pure check(x) returns (uint256 n) {
+        if (x < SCALING) {
+            revert Argon__LogNegativeResult(x);
         }
         unchecked {}
     }
 
-    function exp2(uint256 x) internal pure check(x) returns (uint256 result) {
-        // 2^119.589411415945044523 = 10^36
-        if (x >= 119_589411_415945_044523) {
-            revert Argon__Exp2InputLarge(x);
+    /// @notice Explain to an end user what this does
+    /// @dev Explain to a developer any extra details
+    /// @param x a parameter just like in doxygen (must be followed by parameter name)
+    /// @return n the return variables of a contract’s function state variable
+    function ln(uint256 x) internal pure check(x) returns (uint256 n) {
+        if (x < SCALING) {
+            revert Argon__LogNegativeResult(x);
         }
         unchecked {}
     }
 
-    function pow(uint256 x, uint256 y) internal pure check(x) check(y) returns (uint256 result) {}
+    /// @notice Explain to an end user what this does
+    /// @dev Explain to a developer any extra details
+    /// @param x a parameter just like in doxygen (must be followed by parameter name)
+    /// @return n the return variables of a contract’s function state variable
+    function exp(uint256 x) internal pure returns (uint256 n) {}
 
-    function sqrtInt(uint256 x) public pure returns (uint256 y) {
-        // Newton's algorithm to find integer sqaure root of x
-        // Outperforms Babylonian Method in gas cost as long as x > 7*1e9
-        // For 18 decimal fixed point numbers, this algo will always outperform
+    /// @notice Explain to an end user what this does
+    /// @dev Explain to a developer any extra details
+    /// @param x a parameter just like in doxygen (must be followed by parameter name)
+    /// @return n the return variables of a contract’s function state variable
+    function bexp(uint256 x) internal pure returns (uint256 n) {}
+
+    /// @notice Explain to an end user what this does
+    /// @dev Explain to a developer any extra details
+    /// @param x a parameter just like in doxygen (must be followed by parameter name)
+    /// @return n the return variables of a contract’s function state variable
+    function _lbfloor(uint256 x) private pure returns (uint256 n) {
         unchecked {
-            // find y such that y=2^m, such that 2^m <= sqrt(n) < 2^(m+1)
-            uint256 xReducer = x;
-            y = 1;
-            for (uint8 i = 128; i >= 2; i /= 2) {
-                if (xReducer >= 2**i) {
-                    xReducer >>= i;
-                    y <<= (i / 2);
-                }
+            if (x >= 0x10000000000000000) {
+                x >>= 64;
+                n += 64;
             }
-            // repeat y=(y+x/y)/2 until y>=x
-            for (uint8 j = 0; j < 7; j++) {
-                y = (y + x / y) >> 1;
+            if (x >= 0x100000000) {
+                x >>= 32;
+                n += 32;
             }
-            return y >= (x / y) ? (x / y) : y;
+            if (x >= 0x10000) {
+                x >>= 16;
+                n += 16;
+            }
+            if (x >= 0x100) {
+                x >>= 8;
+                n += 8;
+            }
+            if (x >= 0x10) {
+                x >>= 4;
+                n += 4;
+            }
+            if (x >= 0x4) {
+                x >>= 2;
+                n += 2;
+            }
+            if (x >= 0x2) {
+                n += 1;
+            }
         }
     }
 
-    function _msb(uint256 x) internal pure returns (uint256 msb) {
-        if (x >= 2**128) {
-            x >>= 128;
-            msb += 128;
-        }
-        if (x >= 2**64) {
-            x >>= 64;
-            msb += 64;
-        }
-        if (x >= 2**32) {
-            x >>= 32;
-            msb += 32;
-        }
-        if (x >= 2**16) {
-            x >>= 16;
-            msb += 16;
-        }
-        if (x >= 2**8) {
-            x >>= 8;
-            msb += 8;
-        }
-        if (x >= 2**4) {
-            x >>= 4;
-            msb += 4;
-        }
-        if (x >= 2**2) {
-            x >>= 2;
-            msb += 2;
-        }
-        if (x >= 2**1) {
-            msb += 1;
+    /// @notice Explain to an end user what this does
+    /// @dev Explain to a developer any extra details
+    /// @param x a parameter just like in doxygen (must be followed by parameter name)
+    /// @return n the return variables of a contract’s function state variable
+    function _sqrtinit(uint256 x) private pure returns (uint256 n) {
+        unchecked {
+            n = 2**(_lbfloor(x) / 2);
         }
     }
 }
